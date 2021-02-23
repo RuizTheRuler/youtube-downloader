0cf4bbe9293b64a403c79b53a766c071808e54d6
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-23 01:17:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from pytube import YouTube
+from flask import Flask
 
-link = input('Enter the YouTube link: ')
-path = input('Enter the path to save the video: ')
-yt = YouTube(link)
+app = Flask(__name__)
 
-print(yt.title)
-print(yt.description)
-print()
-choice = yt.streams.filter(progressive = True).get_highest_resolution()
-print(choice)
+@app.route("/")
+def index():
 
-print('Downloading...')
-ys.download()
-print('Download Completed')
+  link = input('Enter the YouTube link: ')
+  path = input('Enter the path to save the video: ')
+  yt = YouTube(link)
+
+  print(yt.title)
+  print(yt.description)
+  print()
+  choice = yt.streams.filter(progressive = True).get_highest_resolution()
+  print(choice)
+
+  print('Downloading...')
+  ys.download()
+  print('Download Completed')

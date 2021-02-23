ee62b18ab14a962a0f041351a4945d290074dfbf
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-23 00:52:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from pytube import YouTube
 
-link = input('Enter the link: ')
+link = input('Enter the YouTube link: ')
+path = input('Enter the path to save the video: ')
 yt = YouTube(link)
 
-title = yt.title
-desc = yt.description
+print(yt.title)
+print(yt.description)
+print()
+choice = yt.streams.filter(progressive = True).get_highest_resolution()
+print(choice)
+
+print('Downloading...'')
+ys.download()
+print('Download Completed')
